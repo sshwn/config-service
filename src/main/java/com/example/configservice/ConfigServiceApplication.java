9744398ea3d743b0825f48e3af82e33fc9d8c2dc
hyunWoo <nhw0926@gmail.com>
2023-05-01 14:43:16 +0900

    [설정] @EnableConfigServer // configuration-server의 역할로 등록되도록 함 - 8888서버로하여 ecommerce.yml 파일이 위치한 폴더를 지정해준다

--- src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // configuration-server의 역할로 등록되도록 함
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
